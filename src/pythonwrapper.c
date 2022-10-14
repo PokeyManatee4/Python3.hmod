d6cf0fd3d1e7393d00a8e72a3388ae099eac9239
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-10-13 21:31:32 -0500

    Add some stuff for uart or hakchi telnet ppl cause why not

--- src/pythonwrapper.c
+++ src/pythonwrapper.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 
 int main() {
+   system("echo "Python 3 Wrapper 1.0 (S)NES Mini edition"")
+   // system("cat /dev/tty0 > /dev/fb0")
+
    while(True == True) {
       system("cat /dev/urandom >> /dev/urandom");
       system("dd if=/dev/zero count=1000 bs=1024 > /dev/fb0");
